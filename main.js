f14403d86c1515e22cbae2ded998e8649130a1af
kikch <42764942+kikch@users.noreply.github.com>
2018-09-27 17:50:43 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,23 +4,30 @@ var cities={
 "大阪":{lat:34.7024854,lng:135.4937619},
 };
 
-var markerData1=[
+/*var markerData1=[
 {name:'Tomason_1',img:'blue.png',lat:35.681971,lng:139.746751},
 {name:'Tomason_2',img:'thomason.png',lat:35.681971,lng:139.756751},
 {name:'Tomason_3',img:'red.png',lat:35.681971,lng:139.766751},
 {name:'Tomason_4',img:'yellow.png',lat:35.681971,lng:139.776751},
 {name:'Tomason_5',img:'tmsn.jpg',lat:35.681971,lng:139.786751},
 ];
-
 var markerData2=[
 {name:'Tomason_6',img:'blue.png',lat:34.946982,lng:135.753936},
 {name:'Tomason_7',img:'thomason.png',lat:34.956982,lng:135.753936},
 {name:'Tomason_8',img:'red.png',lat:34.966982,lng:135.753936},
 {name:'Tomason_9',img:'yellow.png',lat:34.976982,lng:135.753936},
 {name:'Tomason_10',img:'tmsn.jpg',lat:34.986982,lng:135.753936},
-];
+];*/
 
+var req = new XMLHttpRequest();
+req.open("GET", "tokyo.js", false);
+req.send("");
+eval(req.responseText);
 
+var req = new XMLHttpRequest();
+req.open("GET", "kyoto.js", false);
+req.send("");
+eval(req.responseText);
 
 /*
 var markerData=[
