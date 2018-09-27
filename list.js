83b3e3683eef8ba12559512f8cc3fca1eb0c4625
kikch <42764942+kikch@users.noreply.github.com>
2018-09-27 19:31:34 +0900

    Update list.js

--- list.js
+++ list.js
@@ -1,11 +1,11 @@
 /*Tokyo*/
 
 var markerData1=[
-{name:'Tomason_1',img:'blue.png',lat:35.681971,lng:139.746751},
-{name:'Tomason_2',img:'thomason.png',lat:35.681971,lng:139.756751},
-{name:'Tomason_3',img:'red.png',lat:35.681971,lng:139.766751},
-{name:'Tomason_4',img:'yellow.png',lat:35.681971,lng:139.776751},
-{name:'Tomason_5',img:'tmsn.jpg',lat:35.681971,lng:139.786751},
+{star:'★★★☆☆',categ:'stair',img:'blue.png',lat:35.681971,lng:139.746751},
+{star:'★★★☆☆',categ:'window',img:'thomason.png',lat:35.681971,lng:139.756751},
+{star:'★★★☆☆',categ:'window',img:'red.png',lat:35.681971,lng:139.766751},
+{star:'★★★☆☆',categ:'window',img:'yellow.png',lat:35.681971,lng:139.776751},
+{star:'★★★☆☆',categ:'stair',img:'tmsn.jpg',lat:35.681971,lng:139.786751},
 ];
 
 
@@ -13,9 +13,9 @@ var markerData1=[
 /*Kyoto*/
 
 var markerData2=[
-{name:'Tomason_6',img:'blue.png',lat:34.946982,lng:135.753936},
-{name:'Tomason_7',img:'thomason.png',lat:34.956982,lng:135.753936},
-{name:'Tomason_8',img:'red.png',lat:34.966982,lng:135.753936},
-{name:'Tomason_9',img:'yellow.png',lat:34.976982,lng:135.753936},
-{name:'Tomason_10',img:'tmsn.jpg',lat:34.986982,lng:135.753936},
+{star:'★★★☆☆',categ:'window',img:'blue.png',lat:34.946982,lng:135.753936},
+{star:'★★★☆☆',categ:'stair',img:'thomason.png',lat:34.956982,lng:135.753936},
+{star:'★★★☆☆',categ:'window',img:'red.png',lat:34.966982,lng:135.753936},
+{star:'★★★☆☆',categ:'window',img:'yellow.png',lat:34.976982,lng:135.753936},
+{star:'★★★☆☆',categ:'window',img:'tmsn.jpg',lat:34.986982,lng:135.753936},
 ];
