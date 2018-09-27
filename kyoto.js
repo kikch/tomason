8927f37def6c221c7ae2147700d674e780a3ebd6
kikch <42764942+kikch@users.noreply.github.com>
2018-09-27 17:45:59 +0900

    Add files via upload

---
+++ kyoto.js
@@ -0,0 +1,7 @@
+var markerData2=[
+{name:'Tomason_6',img:'blue.png',lat:34.946982,lng:135.753936},
+{name:'Tomason_7',img:'thomason.png',lat:34.956982,lng:135.753936},
+{name:'Tomason_8',img:'red.png',lat:34.966982,lng:135.753936},
+{name:'Tomason_9',img:'yellow.png',lat:34.976982,lng:135.753936},
+{name:'Tomason_10',img:'tmsn.jpg',lat:34.986982,lng:135.753936},
+];
---
+++ tokyo.js
@@ -0,0 +1,7 @@
+var markerData1=[
+{name:'Tomason_1',img:'blue.png',lat:35.681971,lng:139.746751},
+{name:'Tomason_2',img:'thomason.png',lat:35.681971,lng:139.756751},
+{name:'Tomason_3',img:'red.png',lat:35.681971,lng:139.766751},
+{name:'Tomason_4',img:'yellow.png',lat:35.681971,lng:139.776751},
+{name:'Tomason_5',img:'tmsn.jpg',lat:35.681971,lng:139.786751},
+];
