7f638578008446a32b276a707a0678b35a686a55
kikch <42764942+kikch@users.noreply.github.com>
2018-09-27 22:47:57 +0900

    Add files via upload

--- list.js
+++ list.js
@@ -1,11 +1,11 @@
 /*Tokyo*/
 
 var markerData1=[
-{star:'★★★☆☆',categ:'&#35;stairs',img:'blue.png',lat:35.681971,lng:139.746751},
-{star:'★★★☆☆',categ:'&#35;window',img:'thomason.png',lat:35.681971,lng:139.756751},
-{star:'★★★☆☆',categ:'&#35;doors',img:'red.png',lat:35.681971,lng:139.766751},
-{star:'★★★☆☆',categ:'&#35;window',img:'yellow.png',lat:35.681971,lng:139.776751},
-{star:'★★★☆☆',categ:'&#35;stairs',img:'tmsn.jpg',lat:35.681971,lng:139.786751},
+{star:'★★★☆☆',categ:'&#35;stairs',img:'1.jpg',lat:35.681971,lng:139.746751},
+{star:'★★★☆☆',categ:'&#35;window',img:'1.jpg',lat:35.671971,lng:139.756751},
+{star:'★★★☆☆',categ:'&#35;doors',img:'1.jpg',lat:35.661971,lng:139.766751},
+{star:'★★★☆☆',categ:'&#35;window',img:'1.jpg',lat:35.691971,lng:139.776751},
+{star:'★★★☆☆',categ:'&#35;stairs',img:'1.jpg',lat:35.641971,lng:139.786751},
 ];
 
 
@@ -13,9 +13,9 @@ var markerData1=[
 /*Kyoto*/
 
 var markerData2=[
-{star:'★★★☆☆',categ:'&#35;window',img:'blue.png',lat:34.946982,lng:135.753936},
-{star:'★★★☆☆',categ:'&#35;stairs',img:'thomason.png',lat:34.956982,lng:135.753936},
-{star:'★★★☆☆',categ:'&#35;window',img:'red.png',lat:34.966982,lng:135.753936},
-{star:'★★★☆☆',categ:'&#35;window',img:'yellow.png',lat:34.976982,lng:135.753936},
-{star:'★★★☆☆',categ:'&#35;doors',img:'tmsn.jpg',lat:34.986982,lng:135.753936},
+{star:'★★★☆☆',categ:'&#35;window',img:'1.jpg',lat:34.946982,lng:135.753936},
+{star:'★★★☆☆',categ:'&#35;stairs',img:'1.jpg',lat:34.956982,lng:135.763936},
+{star:'★★★☆☆',categ:'&#35;window',img:'1.jpg',lat:34.966982,lng:135.743936},
+{star:'★★★☆☆',categ:'&#35;window',img:'1.jpg',lat:34.976982,lng:135.723936},
+{star:'★★★☆☆',categ:'&#35;doors',img:'1.jpg',lat:34.986982,lng:135.763936},
 ];
